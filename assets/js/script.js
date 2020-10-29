f1792c983d826c71bb708fcbe8631c8a8cb1a2a8
CassBut <clhurlbut@gmail.com>
2020-10-29 14:17:37 -0700

    completed drag and drop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -186,6 +186,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -205,7 +206,15 @@ var dropTaskHandler = function(event) {
   else if (statusType === "tasks-completed") {
     statusSelectEl.selectedIndex = 2;
   }
+  dropZoneEl.removeAttribute("style");
   dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+if (taskListEl) {
+  taskListEl.removeAttribute("style");
+}
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -213,3 +222,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
